f27681f2df427ccf61879ab23cc99f290498b294
Andy Eshiet <andyeshiet@live.com>
2017-08-19 20:47:55 +0100

    Nav bar for about Page now clicks

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
                                 <a href="#">Home</a>
                             </li>
                             <li>
-                                <a href="about.php">About</a>
+                                <a href="http://hng.fun/about.php">About</a>
                             </li>
                             <li>
                                 <a href="#">Participants</a>
