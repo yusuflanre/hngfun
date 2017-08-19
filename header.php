1141f105004c8f540c91f9963d8e9278fe510c89
Nsikaktopdown <nsikakthompson73@gmail.com>
2017-08-19 20:33:11 +0100

    linked the about page to the nav

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
                                 <a href="#">Home</a>
                             </li>
                             <li>
-                                <a href="#">About</a>
+                                <a href="about.php">About</a>
                             </li>
                             <li>
                                 <a href="#">Participants</a>
