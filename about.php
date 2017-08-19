99b9a2563aeb4aaea0d98eccd5a9839a7730b4e2
Nsikaktopdown <nsikakthompson73@gmail.com>
2017-08-19 19:06:49 +0100

    Added center css style

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php include ('header.php'); ?>
+<!--Banner Content-->
+<div id="banner-content" class="row clearfix">
+
+    <div class="col-38">
+
+        <div class="section-heading">
+            <h1>Become a better Software Developer</h1>
+            
+        </div>
+
+        
+    </div>
+
+</div><!--End of Row-->
+</header>
+<?php include ('footer.php'); ?>
