213e9153040afc9799de822283f784c4d31d163a
Peter Pere <perezpeter32@gmail.com>
2017-08-20 19:34:44 +0100

    edit project page

--- project.php
+++ project.php
@@ -2,12 +2,14 @@
 
 
 
-
+<div style="height: 300px; width: 100%;">
 <center>
 
-<h1>Welcome to HNG FUN Projects Page</h1>
+<h1 style="margin-top: 100px;">Welcome to HNG FUN Projects Page</h1>
 
 </center>
+
+</div>
 
 
 
