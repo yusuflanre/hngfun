b3d2db23f631c0e067297a0ae11d51c62b2ae110
Peter Pere <perezpeter32@gmail.com>
2017-08-20 19:31:48 +0100

    edit link to projects page

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
                                 <a href="#">Participants</a>
                             </li>
                             <li>
-                                <a href="#">Projects</a>
+                                <a href="project.php">Projects</a>
                             </li>
                             <li>
                                 <a href="contact.php">Contact Us</a>
