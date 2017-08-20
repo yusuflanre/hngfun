233ff78d1ea3255933eb3b0404ee9ecc95cabb09
Simeon <adegbolasimeon@gmail.com>
2017-08-20 18:32:24 +0200

    pointed contact link to contact page

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
                                 <a href="#">Projects</a>
                             </li>
                             <li>
-                                <a href="#">Contact Us</a>
+                                <a href="contact.php">Contact Us</a>
                             </li>
                             <li>
                                 <a href="#">Past Internship</a>
