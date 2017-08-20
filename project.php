fe86a4b0aa20dea15a8856e07b49420c2f4591a5
Peter Pere <perezpeter32@gmail.com>
2017-08-20 21:39:20 +0100

    Add codes to projects.php

--- project.php
+++ project.php
@@ -112,6 +112,106 @@
 
 			</div>
 		</div>
+
+		<div class="p-card">
+			<div class="p-card-photo"><img src="https://www.w3schools.com/css/img_fjords.jpg" alt=""></div>
+			<div class="p-card-info">
+				<a href="#"><h3 class="p-card-title">Build Hng.fun website</h3></a>
+				<p class="p-card-description">Brief description of the project goes here. Brief brief yes description of the project goes here...</p>
+			</div>
+			<div class="p-card-footer">
+				<div class="p-footer-separate">
+					
+						<span class="dot">&#9679; </span> All Teams
+					
+				</div>
+				<div class="p-footer-separate">
+					
+						<span class="dot2">&#9679; </span> PHP &nbsp; &nbsp;
+					
+					
+						
+						<span class="dot3">&#9679; </span> 29 Aug, 17 &nbsp; &nbsp;
+					
+				</div>
+
+			</div>
+		</div>
+
+		<div class="p-card">
+			<div class="p-card-photo"><img src="https://www.w3schools.com/css/img_fjords.jpg" alt=""></div>
+			<div class="p-card-info">
+				<a href="#"><h3 class="p-card-title">Build Hng.fun website</h3></a>
+				<p class="p-card-description">Brief description of the project goes here. Brief brief yes description of the project goes here...</p>
+			</div>
+			<div class="p-card-footer">
+				<div class="p-footer-separate">
+					
+						<span class="dot">&#9679;</span> All team
+					
+				</div>
+				<div class="p-footer-separate">
+					
+						<span class="dot2">&#9679;</span> PHP &nbsp; &nbsp;
+					
+					
+						
+						<span class="dot3">&#9679;</span> 29 Aug, 17 &nbsp; &nbsp;
+					
+				</div>
+
+			</div>
+		</div>
+
+		<div class="p-card">
+			<div class="p-card-photo"><img src="https://www.w3schools.com/css/img_fjords.jpg" alt=""></div>
+			<div class="p-card-info">
+				<a href="#"><h3 class="p-card-title">Build Hng.fun website</h3></a>
+				<p class="p-card-description">Brief description of the project goes here. Brief brief yes description of the project goes here...</p>
+			</div>
+			<div class="p-card-footer">
+				<div class="p-footer-separate">
+					
+						<span class="dot">&#9679;</span> All team
+					
+				</div>
+				<div class="p-footer-separate">
+					
+						<span class="dot2">&#9679;</span> PHP &nbsp; &nbsp;
+					
+					
+						
+						<span class="dot3">&#9679;</span> 29 Aug, 17 &nbsp; &nbsp;
+					
+				</div>
+
+			</div>
+		</div>
+
+		<div class="p-card">
+			<div class="p-card-photo"><img src="https://www.w3schools.com/css/img_fjords.jpg" alt=""></div>
+			<div class="p-card-info">
+				<a href="#"><h3 class="p-card-title">Build Hng.fun website</h3></a>
+				<p class="p-card-description">Brief description of the project goes here. Brief brief yes description of the project goes here...</p>
+			</div>
+			<div class="p-card-footer">
+				<div class="p-footer-separate">
+					
+						<span class="dot">&#9679;</span> All Team
+					
+				</div>
+				<div class="p-footer-separate">
+					
+						<span class="dot2">&#9679;</span> PHP &nbsp; &nbsp;
+					
+					
+						
+						<span class="dot3">&#9679;</span> 29 Aug, 17 &nbsp; &nbsp;
+					
+				</div>
+
+			</div>
+		</div>
 	</div>
 
 
