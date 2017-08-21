e543d6cb8b8d4251bad26ffbc6d25e773f89f61e
Eniwoke Cornelius <eniwokepdm@gmail.com>
2017-08-22 00:41:40 +0100

    fixed some word mistakes

--- about.php
+++ about.php
@@ -29,8 +29,8 @@
             <table>
                 <tr>
                     <th>Founded in<p class="thick">2016</p><p class="light">Hng internship was founded in 2016 to develop world class developers in Nigeria.</p></th>
-                    <th>Track spend <p class="thick">$13 million+</p><p class="light"> They eventually get placed into companies that are looking for software developers.</p></th>
-                     <th>Trained Developer <p class="thick">5000+</p><p class="light">We get people who are interested in technology and simulate a working environment.</p></th>
+                    <th>Track spend <p class="thick">$13 million+</p><p class="light"> They eventually get placed into companies searching for software developers.</p></th>
+                     <th>Trained Developer <p class="thick">5000+</p><p class="light">We get people who are interested in technology and simulate a work environment.</p></th>
                 </tr>
 
 
