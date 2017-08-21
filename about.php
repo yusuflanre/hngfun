6de965a019407dd47b4c549228bf688e272888d1
komsic <kayodemicheal159@gmail.com>
2017-08-21 13:19:44 +0100

    updated task 1 of about.php

--- about.php
+++ about.php
@@ -13,20 +13,34 @@
 
 </div><!--End of Row-->
 </header>
+
+<div style="max-width: 50vw; margin: auto; text-align: center;padding-bottom: 20px;">
+    <img src="http://res.cloudinary.com/komsic/image/upload/v1503308825/Group_53_elr2ke.png" width="150px" height="150px"> <br>
+    <h2> <strong> Our Mission</strong> </h2>
+    <p style="text-align: justify;"> We get people who are intrested in technology and simulate a working enviroment where they
+    have to tackle real work issues. With minimal guidance, instinct kicks in and the best ones 
+    come out of the lot and start the second phase of the internship where the tasks get
+    increasingly harder.</p>
+</div>
+
 <div class="center">
 
-    <div class="section-1 margin"  >We are on a mission of developing world class software Engineers</div>
-    <div class="body">
-        <div class="section-1-body"> Internship is for 500 interns - The Hotels.ng Software Developers Internship Programme To last for 4 months starting from the end of September 2017.
-            The internship is targeted at young Nigerians in Akwa Ibom who want to be software developers and contribute to the growth of technology in Nigeria.</div>
-
-
-
-	<div class="section-1 margin"> Join Us </div> 
-          <div class="section-1-body"> Join the team of our future world class developers.</br>
-	   <button class="button">JOIN TEAM</button>
-          </div>
-
-            </div>
+    <div class="section-1 margin"  >We are on a mission of developing world class software Engineers
     </div>
+
+    <div class="body">
+        <div class="section-1-body"> Internship is for 500 interns - The Hotels.ng Software 
+        Developers Internship Programme To last for 4 months starting from the end of September 
+        2017. The internship is targeted at young Nigerians in Akwa Ibom who want to be software
+        developers and contribute to the growth of technology in Nigeria.
+        </div>
+
+    	<div class="section-1 margin"> Join Us 
+        </div> 
+
+        <div class="section-1-body"> Join the team of our future world class developers.<br>
+    	   <button class="button">JOIN TEAM</button>
+        </div>
+    </div>
+</div>
 <?php include ('footer.php'); ?>
