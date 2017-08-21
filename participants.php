17971ebf4ed8988b6322306fffa946bccaa4b75a
Babatunde Adedolapo <dolapob@gmail.com>
2017-08-21 22:09:21 +0100

    Update

--- participants.php
+++ participants.php
@@ -753,4 +753,34 @@
     
 
 </div>
+					    
+					       
+<div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url(https://ca.slack-edge.com/T3QLSP8HM-U6Q4F9V0E-45a4b4ef6a53-512); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+
+
+            <a href="http://hng.fun/profile/dolapobabs.html">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Babatunde Adedolapo</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">dolapob@gmail.com</p>
+            <!--Email-->
+        </div>
+
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"> Team 3 </h3>
+                <!--Team-->
+            </a>
+
+
+
+        </div>
+
+    </div>
+
+</div>
 <?php  include "footer.php" ?>
