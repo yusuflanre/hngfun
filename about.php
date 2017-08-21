5e7aaaf9dd59d28eb5ae82b0e9ac1f6f110a949a
Nsikaktopdown <nsikakthompson73@gmail.com>
2017-08-21 12:52:58 +0100

    Updated about page

--- about.php
+++ about.php
@@ -12,20 +12,31 @@
     </div>
 
 </div><!--End of Row-->
-</header>
+</header>   
+    <div>
+        <table>
+            <tr>
+                <th>Founded in<p class="thick">2016</p><p class="light">Hng internship was founded in 2016 to develop world class developer in Nigeria</p></th>
+                <th>Track spend <p class="thick">$13 million+</p><p class="light"> They eventually get placed into companies that are looking for software developers.</p></th>
+                 <th>Trained Developer <p class="thick">5000+</p><p class="light">We get people who are interested in technology and simulate a working environment </p></th>
+            </tr>
+ 
+  
+        </table>
 
+</div>
 
             <div class="row">
                 <div class="image"><img src="images/markessien.png" alt=""></div>
-                <div class="text">
-                    <div class="name-text">Mark Essien</div>
-                    <div class="name-post-text">CEO/Founder</div>
-                    <div class="name-body-text">I'm Mark Essien, born and grew up in the small town of Ikot Ekpene, Akwa Ibom State, Nigeria.
+                    <div class="text">
+                        <div class="name-text">Mark Essien</div>
+                        <div class="name-post-text">CEO/Founder</div>
+                        <div class="name-body-text">I'm Mark Essien, born and grew up in the small town of Ikot Ekpene, Akwa Ibom State, Nigeria.
 I did my primary school at Monika Kindergarten, and my secondary school at Federal Government, Ikot Ekpene. As you can tell, very much a small town upbringing.
 After that, I moved to Germany to attend University. It was an interesting experience, but after many years in Germany, I decided that I prefer to live in Nigeria.</div>
-<div class="linkedin"><img src="images/linkedin.png" height="36" width="36" alt="" align="left"></div>            
-</div>
+                        <div class="linkedin"><img src="images/linkedin.png" height="36" width="36" alt="" align="left"></div>            
+                        </div>
                      
+                </div>
             </div>
-    </div>
 <?php include ('footer.php'); ?>
