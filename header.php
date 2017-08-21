3abb68881e3638a440e23abfd752a1395f21d9c9
holumiede <osideindeo@gmail.com>
2017-08-21 18:36:57 +0100

    upload

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head lang="en">
     <meta charset="UTF-8">
 
@@ -7,7 +8,7 @@
 
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Favicon-->
     <link rel="stylesheet" href="css/style.css">
@@ -22,51 +23,52 @@
     <!--Google Webfonts-->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 </head>
+
 <body class="participants">
 
-<div id="wrapper">
-    <header id="banner" class="scrollto clearfix" data-enllax-ratio=".5">
-        <div id="header" class="nav-collapse">
-            <div class="row clearfix">
-                <div class="col-1">
-                    <div id="logo">
-                        <h3>hng.fun</h3>
+    <div id="wrapper">
+        <header id="banner" class="scrollto clearfix" data-enllax-ratio=".5">
+            <div id="header" class="nav-collapse">
+                <div class="row clearfix">
+                    <div class="col-1">
+                        <div id="logo">
+                            <h3>hng.fun</h3>
+                        </div>
+                        <nav id="nav-main">
+                            <ul>
+                                <li>
+                                    <a href="#">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.php">About</a>
+                                </li>
+                                <li>
+                                    <a href="participants.php">Participants</a>
+                                </li>
+                                <li>
+                                    <a href="#">Projects</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact Us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Past Internship</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                                <li>
+                                    <a href="#">Supporters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Library</a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <div id="nav-trigger"><span></span></div>
+                        <nav id="nav-mobile"></nav>
+
                     </div>
-                    <nav id="nav-main">
-                        <ul>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="about.php">About</a>
-                            </li>
-                            <li>
-                                <a href="#">Participants</a>
-                            </li>
-                            <li>
-                                <a href="#">Projects</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="#">Past Internship</a>
-                            </li>
-                            <li>
-                                <a href="#">News</a>
-                            </li>
-                            <li>
-                                <a href="#">Supporters</a>
-                            </li>
-                            <li>
-                                <a href="#">Library</a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    <div id="nav-trigger"><span></span></div>
-                    <nav id="nav-mobile"></nav>
-
                 </div>
             </div>
-        </div>
