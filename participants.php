3f47430de73429731640af6d202cd1a91b7cf1e8
Hameed Ayomide <hameedayomide@gmail.com>
2017-08-21 23:34:49 +0100

    Added devayo to participants page

--- participants.php
+++ participants.php
@@ -23,6 +23,27 @@
 
         <!--  Each Participant starts here  -->
 
+    <div class="p-card">
+        <div class="v-center">
+            <div class="p-card-photo" style="background-image: url(https://image.ibb.co/b1H445/pic.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/devayo">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Hameed Abdul-lateef</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">Hameedayomide@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 5 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+        </div>
+    </div>
+
 
 
  <!--  Each Participant starts here  -->
