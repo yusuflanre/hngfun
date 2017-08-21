7f88b0c99cb80ef018004d5626681b0e9a0c8978
TobiLight <jgetitdonefast@gmail.com>
2017-08-21 20:56:27 +0100

    Added my name to the Participants.php page

--- participants.php
+++ participants.php
@@ -222,6 +222,28 @@
 
         </div>
 
+		<!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/tobilight/image/upload/v1503321342/IMAG0082_fuubsp.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/tobilight.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Agunloye Oluwatobiloba Light</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">jgetitdonefast@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 8 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!--  Each Participants ends here  -->
 
 
 
