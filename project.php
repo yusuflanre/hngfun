20d1474bf4438cdd09defec84b09bfd1f93c15f0
Peter Pere <perezpeter32@gmail.com>
2017-08-21 18:57:30 +0100

    Change page title

--- project.php
+++ project.php
@@ -1,4 +1,20 @@
-<?php include ('header.php'); ?>
+<?php 
+
+
+
+ob_start();
+    include("header.php");
+    $buffer=ob_get_contents();
+    ob_end_clean();
+
+    $title = "HNG | Projects";
+    $buffer = preg_replace('/(<title>)(.*?)(<\/title>)/i', '$1' . $title . '$3', $buffer);
+
+    echo $buffer;
+
+
+
+?>
 
 
 <style type="text/css">
