f8da1b57ff27163767a9d78d084bf4d380e99134
Idorenyin Obong <idee4ril@gmail.com>
2017-08-21 12:53:27 +0100

    fixed map about page

--- about.php
+++ about.php
@@ -29,7 +29,9 @@ After that, I moved to Germany to attend University. It was an interesting exper
             </div>
   
 <div class="row">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.719354494831!2d3.3791137583773905!3d6.509017387961078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9ddabc4518c15d14!2sHotels.ng!5e0!3m2!1sen!2sng!4v1503315270517"  height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3964.1000594701727!2d3.3791138!3d6.5090174!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9ddabc4518c15d14!2sHotels.ng!5e0!3m2!1sen!2sng!4v1503315969626" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 </div>
 
 <?php include ('footer.php'); ?>
