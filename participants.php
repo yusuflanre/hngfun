e8e523bf24658282010ed38a88beff56aad64784
Idorenyin Obong <idee4ril@gmail.com>
2017-08-21 22:25:55 +0100

    fixed links

--- participants.php
+++ participants.php
@@ -464,6 +464,35 @@
         </div>
 
 
+ <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://scontent-bru2-1.xx.fbcdn.net/v/t1.0-1/p160x160/14657334_1259677607387062_8116538770825805178_n.jpg?oh=fd7eeb04eb8940e886b79c5b6354e74b&oe=5A277060); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+
+
+                    <a href="http://hng.fun/profile/idee.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Idorenyin Obong</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">idee4ril@gmail.com</p>
+                    <!--Email-->
+                </div>
+
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 2 </h3>
+                        <!--Team-->
+                    </a>
+
+
+
+                </div>
+
+            </div>
+
+
+
 
 
 	 <div class="p-card">
@@ -688,10 +717,7 @@
 
     </div>
        <!--  Each Participants ends here  -->
-<<<<<<< HEAD
 
-=======
->>>>>>> 1d4bd61ab9659a59d9e6c5ee6e1fb4b8a36b41f0
         <div class="p-card">
             <div class="v-center">
                 <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/uhexos/assets/img/profile.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
@@ -731,48 +757,17 @@
                        <h3 class="p-card-title"> Team 1 </h3>
                        <!--Team-->
                    </a>
-<<<<<<< HEAD
 
-=======
->>>>>>> 1d4bd61ab9659a59d9e6c5ee6e1fb4b8a36b41f0
             </div>
 
         </div>
 
     </div>
        <!--  Each Participants ends here  -->
-<<<<<<< HEAD
-		
-
-<!--  Each Participant starts here  -->
-     <div class="p-card">
-         <div class="v-center">
-             <div class="p-card-photo" style="background-image: url(https://scontent-bru2-1.xx.fbcdn.net/v/t1.0-1/p160x160/14657334_1259677607387062_8116538770825805178_n.jpg?oh=fd7eeb04eb8940e886b79c5b6354e74b&oe=5A277060); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
-             <div class="p-card-info">
-                 <a href="http://hng.fun/profile/idee.html">
-                     <!--Profile Link-->
-                     <h3 class="p-card-title">Idorenyin Obong</h3>
-                     <!--Profile Name-->
-                 </a>
-                 <p class="p-card-description">idee4ril@gmail.com</p>
-                 <!--Email-->
-             </div>
-             <div class="p-team">
-                 <a href="#">
-                     <h3 class="p-card-title"> Team 2 </h3>
-                     <!--Team-->
-                 </a>                </div>            </div>        </div>
-     <!--  Each Participants ends here  -->
-
-
 
 
 			       
-					       
-=======
-
-
->>>>>>> 1d4bd61ab9659a59d9e6c5ee6e1fb4b8a36b41f0
+	
         <!--  Paste above this and leave a few lines so the next person can see this -->
 
 
