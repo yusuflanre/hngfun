28da7d52b46c2a5aea97d115166c01922d96ca9f
fucho <fmgbeoma@yahoo.com>
2017-08-22 10:14:12 +0100

    fix conflict

--- header.php
+++ header.php
@@ -15,11 +15,7 @@
 
     <meta name="keywords" content="">
     <meta name="description" content="">
-<<<<<<< HEAD
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
-=======
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
->>>>>>> 971bf31092894dc00997122bb31e6a6ea3acb029
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Favicon-->
     <link rel="stylesheet" href="css/style.css">
@@ -46,12 +42,7 @@
                             <img src="img/hnglogo.png" width="100" alt="hotels ng Internship logo">
                         </a>
                     </div>
-<<<<<<< HEAD
-                    <nav id="nav-main">
-                        <ul>
-                            <li>
-                                <a href="#">Home</a>
-=======
+
 
                     <!-- right nav menu -->
                     <nav class="landing-page__nav--right">
@@ -60,29 +51,13 @@
                                 <a class="landing-page__item--links" href="index.php">
                                     <img src="img/home.png" width="30">
                                 </a>
->>>>>>> 971bf31092894dc00997122bb31e6a6ea3acb029
                             </li>
                             <li class="landing-page__item">
                                 <a class="landing-page__item--links" href="about.php">
                                     <img src="img/about.png" width="30">
                                 </a>
                             </li>
-<<<<<<< HEAD
-                            <li>
-                                <a href="#">Participants</a>
-                            </li>
-                            <li>
-                                <a href="#">Projects</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="#">Past Internship</a>
-                            </li>
-                            <li>
-                                <a href="#">News</a>
-=======
+
                             <li class="landing-page__item">
                                 <a class="landing-page__item--links" href="participants.php">
                                     <img src="img/participants.png" width="30">
@@ -99,7 +74,7 @@
                                 </a>
                             </li>
                             <li class="landing-page__item">
-                                <a class="landing-page__item--links" href="past-internships.php">
+                                <a class="landing-page__item--links" href="past-internships-2.php">
                                     <img src="img/pastinternship.png" width="30">
                                 </a>
                             </li>
@@ -107,29 +82,20 @@
                                 <a class="landing-page__item--links" href="news.php">
                                     <img src="img/news.png" width="30">
                                 </a>
->>>>>>> 971bf31092894dc00997122bb31e6a6ea3acb029
                             </li>
                             <li class="landing-page__item">
                                 <a class="landing-page__item--links" href="#contact">
                                     <img src="img/supporters.png" width="30">
                                 </a>
                             </li>
-<<<<<<< HEAD
-                            <li>
-                                <a href="#">Library</a>
-=======
                             <li class="landing-page__item">
                                 <a class="landing-page__item--links" href="library.php">
                                     <img src="img/library.png" width="30">
                                 </a>
->>>>>>> 971bf31092894dc00997122bb31e6a6ea3acb029
+
                             </li>
                         </ul>
                     </nav>
                     <div class="clear"></div>
                 </div>
             </div>
-<<<<<<< HEAD
-        </div>
-=======
->>>>>>> 971bf31092894dc00997122bb31e6a6ea3acb029
