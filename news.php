3174cbbe781ad6649b775c6896051d639a474eca
lateef <lateef>
2017-08-22 07:20:45 +0100

    fixed issues on news page

--- news.php
+++ news.php
@@ -1,6 +1,6 @@
 <?php  include ('header.php');?>
 <div id="banner-content" class="row clearfix">
-	<h1 style ="margin-left:25px"><i class="fa fa-newspaper-o" style="font-size:48px;"></i>NEWS FEED</h1>
+	<h1><i class="fa fa-newspaper-o" style="font-size:48px;"></i>NEWS FEED</h1>
 </div>
 
 	<div class = "team8-card-container">
@@ -12,7 +12,7 @@
 			</div>	
 			<div class="team8-card-photo" style="background-image: url(https://pbs.twimg.com/profile_images/836907288921276416/ahoX6Eke.jpg); background-size: cover; background-repeat: no-repeat;"></div>
 			<div class="team8-card-info">	
-				<p class="team8-card-description">Hotels NG CEO, Mark Essien took to his twitter page to announce the 2nd internship program for the year,
+				<p class="team8-card-content">Hotels NG CEO, Mark Essien took to his twitter page to announce the 2nd internship program for the year,
 				this announcement came as a result of the current ASUU strike. Mark urged young Nigerian undergraduates to see this as an opportunity 
 				and make use of this time develop themselves with technical skills.<br>
 				The internship program has over 1000+ applicants and still counting.
@@ -20,13 +20,41 @@
 			</div>
 			<div class="team8-card-footer">
 				<h6 class="team8-footer-right">
-					<span class="dot3">&#9679; </span> 29 Aug, 17 &nbsp; &nbsp;
+					<span class="dot3">&#9679; </span> 21 Aug, 17 &nbsp; &nbsp;
 				</h6>	
 			</div>
 		</div>
 		
 		<!-- second news article -->
-			
+		<div class="team8-card">
+			<div class="team8-card-info">
+				<h2 class="team8-card-title">HNG internship projects</h2>
+			</div>	
+			<div class="team8-card-photo" style="background-image: url(http://res.cloudinary.com/dmz79kgf0/image/upload/v1503357350/screen_mmcwcd.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+			<div class="team8-card-info">	
+				<p class="team8-card-content">The internship program kick started with applicants joining the HNG internship slack team, where they were required to update 
+				their profile with their full names, phone numbers, email and profile pictures. With that being done, they were given a set of tasks to complete which would 
+				enable them proceed to the next stage of the internship application. Applicant were required to include their names in the contributors file on the hng-internship 
+				github <a href = "https://github.com/hnginterns/getting-started-h2-2017">repository</a> and also create and push PHP & SQL files to their individual github repositories. 
+				Quite a good number of prospective interns completed the given tasks and posted links for review before they were allowed to proceed to the next stage.
+				<br><br>
+				After the set of tasks in the first stage were completed, prospective interns were split into groups and were given both individual and group task to perform. 
+				The individual task involved them creating their personal profile page and hosting it on the hng.fun/profile website and alot of them came up with very impressive pages which Mark Essien 
+				posted links to in a series of tweets. The group task on the other hand involved collaborating and building this awesome website(hng.fun) which was a bit challenging but the worked together 
+				and pulled through.
+				<br><br>
+				While the interns wait patiently to proceed to the next stage, they add finishing touches and make edits to their projects.
+				</p>
+			</div>
+			<div class="team8-card-footer">
+				<h6 class="team8-footer-right">
+					<span class="dot3">&#9679; </span> 22 Aug, 17 &nbsp; &nbsp;
+				</h6>	
+			</div>
+		</div>
+		
+		<!-- third news article -->
+		
 		</center>	
 	</div>
 		
