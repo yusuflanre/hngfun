378c6de778eebf29a466d7f44d6cfc93eadaddcd
Kingsley Ukeje <30627493+swissking009@users.noreply.github.com>
2017-08-22 01:23:02 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -35,8 +35,8 @@
             <div class="clear"></div>
             <div class="footer-widget">
                 <div class="grid">
-                    <div class="grid__col grid__col_12" style="text-align:center; margin-right:10px;">
-                        <ul class="footer-widget__icons" >
+                    <div class="grid__col grid__col_12" >
+                        <ul class="footer-widget__icons" style="text-align:center; padding-right:20px;" >
                             <li>
                                 <a href=""><img src="img/icons/android.png" width="20"></a>
                             </li>
