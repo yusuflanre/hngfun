6243bae8fb78652022cb581c88da86a38810f734
Adeboga <bogadeji@gmail.com>
2017-08-22 01:40:12 +0100

    Added details to participants.php

--- participants.php
+++ participants.php
@@ -797,7 +797,7 @@
 
 
 
-=======
+
 
 
     <!--  Each Participant starts here  -->
@@ -905,8 +905,7 @@
 
 			       
 	
->>>>>>> b12bc0d8db05e4d170554383a98cee026da681f1
-        <!--  Paste above this and leave a few lines so the next person can see this -->
+
 
 
 
@@ -991,6 +990,29 @@
         </div>
 
     </div>
+
+</div>
+
+<div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://i.imgur.com/aizAwig.png); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/bogadeji">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Adeboga Abigail</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">bogadeji@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 9 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
 
 </div>
 <?php  include "footer.php" ?>
