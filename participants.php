6d87cd5c41f27f2b9cefed858877dd5d17421ed8
its-tayo <akindolietayo@gmail.com>
2017-08-22 01:45:11 +0100

    Name added

--- participants.php
+++ participants.php
@@ -993,4 +993,33 @@
     </div>
 
 </div>
+
+<div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url('https://ca.slack-edge.com/T3QLSP8HM-U6QUTQT2T-6ffc54e0834d-512'); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+
+
+            <a href="http://hng.fun/profile/akindolietayo.html">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Akindolie Oluwatayo</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">akindolietayo@gmail.com</p>
+            <!--Email-->
+        </div>
+
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"> Team 4</h3>
+                <!--Team-->
+            </a>
+
+
+
+        </div>
+
+    </div>
+
+</div>
 <?php  include "footer.php" ?>
