07fe629f831f1435b1139e0e41eb5c0779ef73a6
John Otu <johnne01@gmail.com>
2017-08-22 05:16:30 +0000

    Added John Otu to participants page. Changes to be committed: 	modified:   participants.php

--- participants.php
+++ participants.php
@@ -46,6 +46,27 @@
 
         <div class="p-card">
             <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://s20.postimg.org/bleryh20t/35w_Afqcf_400x400.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/johnotu">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">John Otu</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">johnne01@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 7 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-card">
+            <div class="v-center">
                 <div class="p-card-photo" style="background-image: url(https://avatars2.githubusercontent.com/u/7896429?v=4&s=460
 ); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                 <div class="p-card-info">
