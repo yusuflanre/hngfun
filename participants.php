455494eff52b399d43543eea7aca13a4500dfec0
Oluwabukola Tina <oluwabukolatina2015@gmail.com>
2017-08-22 02:40:18 -0700

    added my name to the participants.php file

--- participants.php
+++ participants.php
@@ -1532,6 +1532,34 @@
             </div>
         </div>
         <!--  Each Participants ends here  -->
+
+
+        <!--  Each Participant starts here  -->
+
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://i.imgur.com/GxwSbib.png); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/oluwabukolatina">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Oluwabukola Tina</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">oluwabukolatina2015@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 1 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!--  Each Participants ends here  -->
         <!--  Each Participant starts here  -->
 
 
