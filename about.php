d4266e63d7b19782c5c95235df8d7221bcfdd0a1
hxvz <omokolataiwo@yahoo.com>
2017-08-22 09:45:40 +0100

    Stash

--- about.php
+++ about.php
@@ -3,53 +3,28 @@
 
     <div class="col-38">
 
-        <div class="section-heading">
+        <!-- <div class="section-heading">
             <h1>Become a better Software Developer</h1>
-        </div>
+        </div> -->
 
     </div>
 
 </div><!--End of Row-->
 </header>
+<div class="section-1">
+    <div id="team-2-section-1">
+        <img src="http://res.cloudinary.com/komsic/image/upload/v1503308825/Group_53_elr2ke.png" width="120px" height="120px"> <br>
 
-<div id="team-2-section-1">
-    <img src="http://res.cloudinary.com/komsic/image/upload/v1503308825/Group_53_elr2ke.png" width="150px" height="150px"> <br>
+         <div class="section-text"> Our Mission</div>
 
-    <h2> <strong> Our Mission</strong> </h2>
+        <div class="section-body-text"> We get people who are intrested in technology and simulate a working enviroment where they
+            have to tackle real work issues. With minimal guidance, instinct kicks in and the best ones 
+            come out of the lot and start the second phase of the internship where the tasks get
+            increasingly harder.</div>
 
-    <p> We get people who are intrested in technology and simulate a working enviroment where they
-    have to tackle real work issues. With minimal guidance, instinct kicks in and the best ones 
-    come out of the lot and start the second phase of the internship where the tasks get
-    increasingly harder.</p>
-
-</div>
-
-<div class="center">
-
-    <div class="section-1 margin"  >We are on a mission of developing world class software Engineers
-    </div>
-
-    <div class="body">
-        <div class="section-1-body"> Internship is for 500 interns - The Hotels.ng Software 
-        Developers Internship Programme To last for 4 months starting from the end of September 
-        2017. The internship is targeted at young Nigerians in Akwa Ibom who want to be software
-        developers and contribute to the growth of technology in Nigeria.
-        </div>
-
-    	<div class="section-1 margin"> Join Us 
-        </div> 
-
-        <div class="section-1-body"> Join the team of our future world class developers.<br>
-    	   <button class="button">JOIN TEAM</button>
-        </div>
     </div>
 </div>
-
-                   <center> <img src="images/group.png" height=150px width=150px>
-                        <h1>Leadership</h1>
-                   </center>
-
-</header>   
+ 
     <div>
         <table>
             <tr>
@@ -62,21 +37,23 @@
         </table>
 
 </div>
-
- <center> <img src="images/group.png" height=150px width=150px>
-                        <h1>Leadership</h1>
-                   </center>
+ <div class="section-leader">
+    <center> <img src="http://res.cloudinary.com/hngfun/image/upload/v1503323007/group_ctikye.png" height=120px width=120px>
+            <div class="section-text">Leadership</div>
+    </center>
+     
+</div>
 
 
             <div class="row">
-                <div class="image"><img src="images/markessien.png" alt=""></div>
+                <div class="image"><img src="http://res.cloudinary.com/hngfun/image/upload/v1503323014/markessien_lg3ey0.png" alt=""></div>
                     <div class="text">
                         <div class="name-text">Mark Essien</div>
                         <div class="name-post-text">CEO/Founder</div>
                         <div class="name-body-text">I'm Mark Essien, born and grew up in the small town of Ikot Ekpene, Akwa Ibom State, Nigeria.
 I did my primary school at Monika Kindergarten, and my secondary school at Federal Government, Ikot Ekpene. As you can tell, very much a small town upbringing.
 After that, I moved to Germany to attend University. It was an interesting experience, but after many years in Germany, I decided that I prefer to live in Nigeria.</div>
-                        <div class="linkedin"><img src="images/linkedin.png" height="36" width="36" alt="" align="left"></div>            
+                        <div class="linkedin"><img src="http://res.cloudinary.com/hngfun/image/upload/v1503323006/linkedin_lquvqx.png" height="36" width="36" alt="" align="left"></div>            
                         </div>
                      
                 </div>
